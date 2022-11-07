56e118b2dd0a1c6fe796eb1e4e7202e1d7c473b6
ShalltearFR <Jossuha@hotmail.fr>
2022-11-07 11:47:48 +0100

    fix head

--- Components/HeadComponent.jsx
+++ Components/HeadComponent.jsx
@@ -10,14 +10,14 @@ export default function HeadComponent(props){
 
             <meta property="og:type" content="website"/>
             <meta property="og:url" content={`https://starwars-research.vercel.app${props.path}`}/>
-            <meta property="og:title" content={`Search - ${props.title}`}/>
+            <meta property="og:title" content={`Search ${props.title}`}/>
             <meta property="og:description" content="Rechercher des informations sur Star Wars"/>
             <meta property="og:image" content="/media/images/logo.png"/>
 
 
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content={`https://starwars-research.vercel.app${props.path}`}/>
-            <meta property="twitter:title" content={`Search - ${props.title}`}/>
+            <meta property="twitter:title" content={`Search ${props.title}`}/>
             <meta property="twitter:description" content="Rechercher des informations sur Star Wars"/>
             <meta property="twitter:image" content="/media/images/logo.png"/>
         </Head>
